6e0c19b54010552422903a18d2be5738516af397
Alonso Serrano <maserrano1995@gmail.com>
2026-03-02 22:30:25 -0600

    add hash password into create user usecase and jwt services

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { env } from "./infrastructure/config/env";
 import { connectMongo } from "./infrastructure/database/mongo.connection";
 import { MongoUserRepository } from "./infrastructure/repositories/mongo-user.repository";
 import { CreateUserUseCase } from "./application/use-cases/create-user";
-
+import { JwtService } from './infrastructure/auth/jwt.service';
 await connectMongo();
 
 const userRepository = new MongoUserRepository();
-const createUserUseCase = new CreateUserUseCase(userRepository);
+const jwtService = new JwtService();
+
+const createUserUseCase = new CreateUserUseCase(userRepository, jwtService);
 
 const app = createServer()
 .use(userController(createUserUseCase));
