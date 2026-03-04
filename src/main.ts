37376c19869746544198c22a7f5c7965e0f163ca
Alonso Serrano <maserrano1995@gmail.com>
2026-03-04 17:21:55 -0600

    add documentation with openapi

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
+import { swagger } from '@elysiajs/swagger';
 import { createServer } from "./interfaces/http/server";
 import { connectMongo } from "./infrastructure/database/mongo.connection";
 import { env } from "./infrastructure/config/env";
 import { UserModule } from "./modules/user/user.module";
 import { ProjectModule } from "./modules/project/project.module";
 import { AuthModule } from "./modules/auth/auth.module";
+import openapi from "../openapi.json"
 
 await connectMongo();
 
 const app = createServer()
   .use(UserModule())
   .use(AuthModule())
-  .use(ProjectModule());
+  .use(ProjectModule())
+  .use(swagger())
+  .use(
+    swagger({
+      documentation: openapi as any,
+       path: "/openapi"
+    })
+  )
+
+  ;
 
 app.listen(env.port);
 
